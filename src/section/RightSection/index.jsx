2cb2df0d76cd55228080ca3acc13a9e9f248b14c
mxwwy <nboonykama@gmail.com>
2026-02-16 01:42:38 +0700

    add about

--- src/section/RightSection/index.jsx
+++ src/section/RightSection/index.jsx
@@ -1,14 +1,15 @@
+import About from "../About"
 const RightSection = () => {
     return(
-       <div>
-        <div>About</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
-        <div className='mb-80'>Experience</div>
+       <div className="grid gap-4 px-5">
+        <About/>
+        <div >Experience</div>
+        <div >Experience</div>
+        <div >Experience</div>
+        <div >Experience</div>
+        <div >Experience</div>
+        <div >Experience</div>
       </div>
     )
 }
-export default RightSection
+export default RightSection;
