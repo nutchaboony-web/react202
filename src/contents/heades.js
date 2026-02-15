eea4327b32e5473303f780bc3a0c33d1cb62254a
mxwwy <nboonykama@gmail.com>
2026-02-16 01:09:33 +0700

    add contents

---
+++ src/contents/heades.js
@@ -0,0 +1,8 @@
+import fileName from '../assets/COM-SCI2565.pdf'
+export const data ={
+    "name":"Nutcha Boonyakama",
+    "title":"Computer Science",
+    "caption":"I build accessible, pixel-perfect digital experiences for the web.",
+    btnText:"View Resume",
+    link:fileName
+}
