7d231dda920d3056466a4c1a181639992aeb94a2
mxwwy <nboonykama@gmail.com>
2026-02-16 00:52:59 +0700

    update Left Right

---
+++ src/section/RightSection/index.jsx
@@ -0,0 +1,14 @@
+const RightSection = () => {
+    return(
+       <div>
+        <div>About</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+        <div className='mb-80'>Experience</div>
+      </div>
+    )
+}
+export default RightSection
